6c8179491599f53b7299fcab49b8869c377b1e1a
Sean Foley <foley.sea@husky.neu.edu>
2019-09-25 14:58:22 -0400

    Main arduino file.

---
+++ VLC.ino
@@ -0,0 +1,13 @@
+#include <bool.h>
+#include <hashset.h>
+#include <vector.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
