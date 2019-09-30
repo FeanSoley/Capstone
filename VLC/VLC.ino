74c354c69d7f7e53c8e8d8ebc7b813242d44a53f
Sean Foley <foley.sea@husky.neu.edu>
2019-09-30 15:55:51 -0400

    Added example for arduino file, fixed many mistakes in the Packet.cpp file mainly having to do with iterating.

--- VLC/VLC.ino
+++ VLC/VLC.ino
@@ -3,11 +3,26 @@
 #include <Transmission.h>
 
 void setup() {
-  // put your setup code here, to run once:
+  // put your setup code here, to run once:  
+  Serial.begin(9600);
+  bool data[24];
+  pinMode(8, INPUT); 
+  
+  Packet dataPacket;
+  dataPacket.setPosition(0);
+  dataPacket.setCode(3);
+  dataPacket.setData(55);
+  dataPacket.encodePacket();
+  dataPacket.getData(data);
 
+  for(int i = 0; i < 24; i++){
+    Serial.print(data[i]);
+  }
+  Serial.print("\n");
+  dataPacket.sendPacket(3, 8);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  
 }
--- libraries/Packet/Packet.cpp
+++ libraries/Packet/Packet.cpp
@@ -8,7 +8,7 @@ void Packet::setPosition(int position)
   int currentValue;
   for(int i = 0; i < positionSize; i++){
     currentValue = bitRead(position,i);
-    _position[i] = currentValue;
+    _position[positionSize-i-1] = currentValue;
   }
 }
 
@@ -17,7 +17,7 @@ void Packet::setCode(int code)
   int currentValue;
   for(int i = 0; i < codeSize; i++){
     currentValue = bitRead(code,i);
-    _code[i] = currentValue;
+    _code[codeSize-i-1] = currentValue;
   }
 }
 
@@ -27,7 +27,7 @@ void Packet::setData(int data)
   int currentValue;
   for(int i = 0; i < dataSize; i++){
     currentValue = bitRead(data,i);
-    _data[i] = currentValue;
+    _data[dataSize-i-1] = currentValue;
   }
 }
 
@@ -55,7 +55,7 @@ void Packet::getData(bool * data)
 }
 
 void Packet::getPacket(bool * packet){
-    int positionStart, codeStart, dataStart, packetEnd, i;
+    int positionStart, codeStart, dataStart, packetEnd, i, packetIter;
     positionStart = 0;
     codeStart = positionStart + positionSize;
     dataStart = codeStart + codeSize;
@@ -65,18 +65,22 @@ void Packet::getPacket(bool * packet){
         packet[i] = _position[i];
     }
     
-    for(int i = codeStart; i < dataStart; i++){
-         packet[i] = _code[i];
+    for(int i = 0; i < codeSize; i++){
+        packetIter = codeStart;
+        packet[packetIter] = _code[i];
+        packetIter++;
     }
     
-    for(int i = dataStart; i < packetEnd; i++){
-         packet[i] = _data[i];
+    for(int i = 0; i < dataSize; i++){
+        packetIter = dataStart;
+        packet[packetIter] = _data[i];
+        packetIter++;
     }
 }
 
 void Packet::encodePacket()
 {
-    int positionStart, codeStart, dataStart, packetEnd, i;
+    int positionStart, codeStart, dataStart, packetEnd, i, packetIter;
     positionStart = 0;
     codeStart = positionStart + positionSize;
     dataStart = codeStart + codeSize;
@@ -85,49 +89,57 @@ void Packet::encodePacket()
     
     
     for(int i = positionStart; i < codeStart; i++){
+        packetIter = 2*i;
         if(_position[i] == 1){
-            _encodedPacket[i] = 1;
-            _encodedPacket[i+1] = 0;
+            _encodedPacket[packetIter] = 1;
+            _encodedPacket[packetIter+1] = 0;
         }
         else{
-            _encodedPacket[i] = 0;
-            _encodedPacket[i+1] = 1;
+            _encodedPacket[packetIter] = 0;
+            _encodedPacket[packetIter+1] = 1;
         }
+        packetIter++;
     }
     
-    for(int i = codeStart; i < dataStart; i++){
+    for(int i = 0; i < codeSize; i++){
+        packetIter = 2*i + codeStart*2;
         if(_code[i] == 1){
-            _encodedPacket[i] = 1;
-            _encodedPacket[i+1] = 0;
+            _encodedPacket[packetIter] = 1;
+            _encodedPacket[packetIter+1] = 0;
         }
         else{
-            _encodedPacket[i] = 0;
-            _encodedPacket[i+1] = 1;
+            _encodedPacket[packetIter] = 0;
+            _encodedPacket[packetIter+1] = 1;
         }
+        packetIter++;
     }
     
-    for(int i = dataStart; i < packetEnd; i++){
+    for(int i = 0; i < dataSize; i++){
+        packetIter = 2*i + dataStart*2;
         if(_data[i] == 1){
-            _encodedPacket[i] = 1;
-            _encodedPacket[i+1] = 0;
+            _encodedPacket[packetIter] = 1;
+            _encodedPacket[packetIter+1] = 0;
         }
         else{
-            _encodedPacket[i] = 0;
-            _encodedPacket[i+1] = 1;
+            _encodedPacket[packetIter] = 0;
+            _encodedPacket[packetIter+1] = 1;
         }
+        packetIter++;
     }
 }
 
 void Packet::sendPacket(float bitRate, int pin)
 {
-    float calcDelay = 1/bitRate;
+    float calcDelay = 1/bitRate*1000;
     for(int i = 0; i < packetSize*2; i++)
     {
         if(_encodedPacket[i] == 1){
+            Serial.print("High\n");
             digitalWrite(pin, HIGH); // sets the digital pin 13 on
             delay(calcDelay);  
         }
         else{
+            Serial.print("Low\n");
             digitalWrite(pin, LOW); // sets the digital pin 13 on
             delay(calcDelay);  
         }
